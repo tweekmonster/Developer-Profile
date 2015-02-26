f92b36b6f3a145a565dbc80b8384ac2fea85c134
Tommy Allen <tommy@tweek.us>
2015-02-26 17:28:22 -0500

    Bumped version

--- warning.js
+++ warning.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function()
 {
     document.getElementById('acknowledge').addEventListener('click', function()
     {
-        localStorage['warningAcknowledged'] = 'v0.1';
+        localStorage['warningAcknowledged'] = 'v0.2';
         window.location.href = 'options.html';
     });
 });
